ccdddd8feb843c536e4dc1079a33cdf71a29ac07
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2017-07-11 21:07:47 +0200

    Update to elemento 0.5.0

--- core/src/main/java/com/intendia/rxgwt/elemento/RxElemento.java
+++ core/src/main/java/com/intendia/rxgwt/elemento/RxElemento.java
@@ -2,19 +2,20 @@ package com.intendia.rxgwt.elemento;
 
 import com.intendia.rxgwt.elemental2.RxElemental2;
 import elemental2.dom.Event;
-import elemental2.dom.EventListener;
 import elemental2.dom.EventTarget;
 import jsinterop.base.Js;
 import org.jboss.gwt.elemento.core.EventType;
-import rx.Emitter;
 import rx.Observable;
 import rx.functions.Func1;
-import rx.subscriptions.Subscriptions;
 
 public interface RxElemento {
 
     static <T extends Event> Observable<T> fromEvent(EventTarget src, EventType<T, ?> type) {
-        return RxElemental2.fromEvent(src, type.name, false).map(Js::cast);
+        return RxElemental2.fromEvent(src, type.name).map(Js::cast);
+    }
+
+    static <T extends Event> Observable<T> fromEvent(EventTarget src, EventType<T, ?> type, boolean useCapture) {
+        return RxElemental2.fromEvent(src, type.name, useCapture).map(Js::cast);
     }
 
     static <T extends Event, V> Observable<V> fromEvent(EventTarget src, EventType<T, ?> type, Func1<? super T, V> fn) {
