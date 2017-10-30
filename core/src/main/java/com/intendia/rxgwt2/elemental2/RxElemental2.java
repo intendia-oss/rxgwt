ee82305233b918d6fc715860d65a524abb2dc978
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2017-10-30 18:03:20 +0100

    Add RxElemental2.fromPromise

--- core/src/main/java/com/intendia/rxgwt2/elemental2/RxElemental2.java
+++ core/src/main/java/com/intendia/rxgwt2/elemental2/RxElemental2.java
@@ -3,7 +3,9 @@ package com.intendia.rxgwt2.elemental2;
 import elemental2.dom.Event;
 import elemental2.dom.EventListener;
 import elemental2.dom.EventTarget;
+import elemental2.promise.IThenable;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 
 /**
  * This is a experimental (breaking changes between versions) API to add elemental2 typed events.
@@ -22,5 +24,23 @@ public interface RxElemental2 {
             source.addEventListener(type, listener, useCapture);
             s.setCancellable(() -> source.removeEventListener(type, listener, useCapture));
         });
+    }
+
+    static <T> Single<T> fromPromise(IThenable<T> promise) {
+        return Single.create(em -> promise.then(success -> {
+                    em.onSuccess(success);
+                    return null;
+                },
+                failure -> {
+                    em.onError(new PromiseRejectedException(failure));
+                    return null;
+                }));
+    }
+
+    class PromiseRejectedException extends RuntimeException {
+        private final transient Object reject;
+        public PromiseRejectedException() { reject = null; }
+        public PromiseRejectedException(Object reject) { this.reject = reject; }
+        public Object getReject() { return reject; }
     }
 }
